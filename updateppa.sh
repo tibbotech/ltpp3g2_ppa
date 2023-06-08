cb5f635b51b4f67432ce5cb24972a773d6d27b12
imcase <hien@tibbo.com>
2023-06-08 14:22:39 +0800

    tibbo-uboot_0.6.1-1_all.deb includes isp-bootseq

--- updateppa.sh
+++ updateppa.sh
@@ -1,14 +1,58 @@
+#---INPUT ARGS
+PPA_FOLDER=$1
+
+
+
+#---COLOR CONSTANTS
+RESET_COLOR=$'\033[0;0m'
+FG_ORANGE_COLOR=$'\033[30;38;5;215m'
+FG_RED_COLOR=$'\e[30;38;5;9m'
+
+
+#---PRINT CONSTANTS
+ERROR_INVALD_OR_NONEXISTING="***${FG_RED_COLOR}ERROR${RESET_COLOR}: invalid or non-existing ppa-folder" 
+
+
+#---OTHER CONSTANTS
 EMAIL="support@tibbo.com"
 
-PPA_FOLDER=$1
+
 
 #check if 1st argument is empty
 if [ -z "$PPA_FOLDER" ]; then
-    echo "Usage: $0 <ppa_folder>"
-    exit 1
+    tput cud1
+
+    READDIALOG1="---:${FG_ORANGE_COLOR}INPUT${RESET_COLOR}: ppa-folder (e.g. u0_6_0): "
+
+    if [[ -z "${PPA_FOLDER}" ]]; then
+        while true
+        do
+            read -p "${READDIALOG1}" PPA_FOLDER
+
+            if [[ -n "${PPA_FOLDER}" ]]; then
+                curr_dir=$(pwd)
+
+                ppa_fpath="${curr_dir}/${PPA_FOLDER}"
+                if [[ -d "${ppa_fpath}" ]]; then
+                    break
+                else
+                    errormsg="\n${ERROR_INVALD_OR_NONEXISTING} '${ppa_fpath}'\n" 
+
+                    echo -e "${errormsg}"
+                fi
+            else
+                tput cuu1
+                tput el
+            fi
+        done
+    fi
+    # echo "Usage: $0 <ppa_folder>"
+    # exit 1
 fi
 
-cd ${PPA_FOLDER}
+
+# Navigate to 'ppa_fpath'
+cd ${ppa_fpath}
 
 # Packages & Packages.gz
 dpkg-scanpackages --multiversion . > Packages
